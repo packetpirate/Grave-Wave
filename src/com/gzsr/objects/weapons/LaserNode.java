442fb54468e669521c5baf522b2c98adc45a9105
packetpirate <packetpirate@gmail.com>
2017-11-07 12:35:37 -0500

    Fixed error in laser barrier collision detection.

--- src/com/gzsr/objects/weapons/LaserNode.java
+++ src/com/gzsr/objects/weapons/LaserNode.java
@@ -11,7 +11,7 @@ import com.gzsr.gfx.particles.Projectile;
 public class LaserNode extends Projectile {
 	private static final Color BARRIER_COLOR = new Color(0xFFA0B9);
 	private static final float BARRIER_WIDTH = 3.0f;
-	private static final double DURABILITY_MAX = 100.0;
+	private static final double DURABILITY_MAX = 10000.0;
 	
 	private LaserNode other;
 	private boolean host;
@@ -45,12 +45,12 @@ public class LaserNode extends Projectile {
 	
 	public void damage(double amnt) {
 		durability -= amnt;
-		other.damage(amnt);
+		if(host) other.damage(amnt);
 	}
 	
 	@Override
 	public boolean isAlive(long cTime) {
-		return (durability > 0);
+		return ((durability > 0) && (host ? other.isAlive(cTime) : true));
 	}
 	
 	@Override
@@ -58,6 +58,6 @@ public class LaserNode extends Projectile {
 		Line barrier = new Line(position.x, position.y,
 								other.getPosition().x, other.getPosition().y);
 		boolean barrierCollision = barrier.intersects(enemy.getCollider());
-		return super.checkCollision(enemy) && barrierCollision;
+		return super.checkCollision(enemy) || barrierCollision;
 	}
 }
