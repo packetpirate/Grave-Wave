33e429400515bfec2726067f0b8e74248c16bcde
packetpirate <packetpirate@gmail.com>
2018-01-10 18:10:47 -0500

    Made item grid smaller. No way we're going to end up with 156 unique items... also made calculation of box spacing automatic instead of hard-coded.

--- src/com/gzsr/states/ShopState.java
+++ src/com/gzsr/states/ShopState.java
@@ -34,8 +34,13 @@ public class ShopState extends BasicGameState implements InputListener {
 	private static float DETAIL_WIDTH = 200.0f;
 	private static float CONTAINER_HEIGHT = Globals.HEIGHT - 110.0f;
 	
-	private static final int ROWS = 12; // The number of rows of item boxes that can fit in the selection container.
-	private static final int COLS = 13; // The number of item boxes that can fit in one row in the selection container.
+	private static final int ROWS = 6; // The number of rows of item boxes that can fit in the selection container.
+	private static final int COLS = 6; // The number of item boxes that can fit in one row in the selection container.
+	private static final float ITEM_BOX_SIZE = 96.0f;
+	private static final float ITEM_BOX_SPACING_H = 20.0f;
+	private static final float ITEM_BOX_SPACING_V = 10.0f;
+	private static final float SELECTION_CONTAINER_PADDING_H = ((SELECTION_WIDTH - ((COLS * ITEM_BOX_SIZE) + (ITEM_BOX_SPACING_H * (COLS - 1)))) / 2);
+	private static final float SELECTION_CONTAINER_PADDING_V = ((CONTAINER_HEIGHT - ((ROWS * ITEM_BOX_SIZE) + (ITEM_BOX_SPACING_V * (ROWS - 1)))) / 2);
 	
 	private Rectangle [][] itemBoxes;
 	private Pair<Integer> selected;
@@ -47,9 +52,9 @@ public class ShopState extends BasicGameState implements InputListener {
 		itemBoxes = new Rectangle[ROWS][COLS]; // 12 rows, 13 columns
 		for(int r = 0; r < ROWS; r++) {
 			for(int c = 0; c < COLS; c++) {
-				float x = (((c * 48.0f) + (c * 12.0f)) + SELECTION_CONTAINER.x + 13.0f);
-				float y = (((r * 48.0f) + (r * 5.0f)) + SELECTION_CONTAINER.y + 13.0f);
-				itemBoxes[r][c] = new Rectangle(x, y, 48.0f, 48.0f);
+				float x = (((c * ITEM_BOX_SIZE) + (c * ITEM_BOX_SPACING_H)) + SELECTION_CONTAINER.x + SELECTION_CONTAINER_PADDING_H);
+				float y = (((r * ITEM_BOX_SIZE) + (r * ITEM_BOX_SPACING_V)) + SELECTION_CONTAINER.y + SELECTION_CONTAINER_PADDING_V);
+				itemBoxes[r][c] = new Rectangle(x, y, ITEM_BOX_SIZE, ITEM_BOX_SIZE);
 			}
 		}
 		
@@ -97,12 +102,12 @@ public class ShopState extends BasicGameState implements InputListener {
 				float y = itemBoxes[r][c].getY();
 				
 				g.setColor(Color.black);
-				g.fillRect(x, y, 48.0f, 48.0f);
+				g.fillRect(x, y, ITEM_BOX_SIZE, ITEM_BOX_SIZE);
 				
-				if((r == 0) && (c < weapons.size())) {
+				if((r == 0) || ((r == 1) && (c < (weapons.size() - COLS)))) {
 					// FIXME: Change so this can load image from any item box.
-					Image img = weapons.get(c).getInventoryIcon();
-					g.drawImage(img, x, y);
+					Image img = weapons.get((r * COLS) + c).getInventoryIcon();
+					img.draw(x, y, 2.0f);
 				}
 				
 				g.setColor(Color.darkGray);
@@ -110,7 +115,7 @@ public class ShopState extends BasicGameState implements InputListener {
 					g.setColor(Color.white);
 					g.setLineWidth(2.0f);
 				}
-				g.drawRect(x, y, 48.0f, 48.0f);
+				g.drawRect(x, y, ITEM_BOX_SIZE, ITEM_BOX_SIZE);
 				g.setLineWidth(1.0f);
 			}
 		}
@@ -121,15 +126,15 @@ public class ShopState extends BasicGameState implements InputListener {
 		g.setColor(Color.darkGray);
 		g.drawRect((DETAIL_CONTAINER.x + 52.0f), (DETAIL_CONTAINER.y + 40.0f), 96.0f, 96.0f);
 		
-		if((selected != null) && (selected.x == 0) && (selected.y < weapons.size())) {
+		if((selected != null) && ((selected.x == 0) || ((selected.x == 1) && (selected.y < (weapons.size() - COLS)))) && (selected.y < weapons.size())) {
 			// FIXME: Change this so portrait can be loaded for any item box.
-			Image portrait = weapons.get(selected.y).getInventoryIcon();
+			Image portrait = weapons.get((selected.x * COLS) + selected.y).getInventoryIcon();
 			portrait.draw((DETAIL_CONTAINER.x + 52.0f), (DETAIL_CONTAINER.y + 40.0f), 2.0f);
 		}
 		
 		// Draw the item's name below its portrait.
 		// FIXME: Change this so item name can be taken from any item box.
-		String itemName = ((selected != null) && (selected.x == 0) && (selected.y < weapons.size())) ? weapons.get(selected.y).getName() : "No Item Selected";
+		String itemName = ((selected != null) && ((selected.x == 0) || ((selected.x == 1) && (selected.y < (weapons.size() - COLS)))) && (selected.y < weapons.size())) ? weapons.get((selected.x * COLS) + selected.y).getName() : "No Item Selected";
 		Calculate.TextWrap(g, itemName, FONT_NORMAL, 
 						   (DETAIL_CONTAINER.x + 10.0f), (DETAIL_CONTAINER.y + 136.0f + FONT_NORMAL.getHeight()), 
 						   180.0f, true, Color.white);
